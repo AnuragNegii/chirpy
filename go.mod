ec02e4440edb2fb7b5cc5c1fca30e8948e1a2eec
Anurag Negi <neginaurag99@gmail.com>
2025-07-03 12:29:29 +0530

    starting a server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AnuragNegii/chirpy
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type newServer http.ServeMux
+
+
+func main(){
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	
+	srvr := &http.Server{
+		Handler: mux,
+		Addr: ":" + port,
+	}
+	fmt.Printf("Starting go Server at port: %v", port)
+	log.Fatal(srvr.ListenAndServe())
+}
