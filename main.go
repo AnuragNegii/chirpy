c9ce143bacaa4e8c133a0408bd06d6a14a810a59
Anurag Negi <neginaurag99@gmail.com>
2025-07-07 21:33:00 +0530

    added a handler to check health

--- main.go
+++ main.go
@@ -18,7 +18,14 @@ func main(){
 		Handler: mux,
 		Addr: ":" + port,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handler)
 	fmt.Printf("Starting go Server at port: %v", port)
 	log.Fatal(srvr.ListenAndServe())
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
