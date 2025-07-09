7bad9de52e8fdd6bb9325de06b20be1e074b0139
Anurag Negi <neginaurag99@gmail.com>
2025-07-09 16:08:19 +0530

    basic validation of chirps

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -29,6 +30,7 @@ func main(){
 	mux.HandleFunc("GET /api/healthz", handler)
 	mux.HandleFunc("GET /admin/metrics", apicfg.writeHandler)
 	mux.HandleFunc("POST /admin/reset", apicfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", hadnle_Chirp)
 	fmt.Printf("Starting go Server at port: %v", port)
 	log.Fatal(srvr.ListenAndServe())
 }
@@ -63,4 +65,30 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileServerHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Metrics reset"))
+}
+
+func hadnle_Chirp(w http.ResponseWriter, r *http.Request){
+	type parameters struct{
+		Body string `json:"body"`
+	}
+	decoder := json.NewDecoder(r.Body) 
+	var params parameters
+	err := decoder.Decode(&params)
+	if err != nil{
+		log.Printf("error decoding parameters: %v", err)
+		http.Error(w, `{"error":"Invalid request body"}`, http.StatusInternalServerError)
+		return
+	}
+	
+	if len(params.Body) > 140{
+		resp := map[string]string{"error": "Chirp is too long"}
+		w.Header().Set("Content-Type", "application/`json:")
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	resp := map[string]bool{"valid":true}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
 }
